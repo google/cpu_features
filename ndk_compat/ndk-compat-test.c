bd66a6762c345d99fc8f73ddd21a5a135882139f
Guillaume Chatelet <gchatelet@google.com>
2019-01-17 16:49:19 +0100

    Fix values display in ndk-compat-test

--- ndk_compat/ndk-compat-test.c
+++ ndk_compat/ndk-compat-test.c
@@ -2,10 +2,10 @@
 #include "cpu-features.h"
 
 int main() {
-  printf("android_getCpuFamily()=%x\n", android_getCpuFamily());
-  printf("android_getCpuFeatures()=%llx\n", android_getCpuFeatures());
-  printf("android_getCpuCount()=%x\n", android_getCpuCount());
+  printf("android_getCpuFamily()=%d\n", android_getCpuFamily());
+  printf("android_getCpuFeatures()=0x%08llx\n", android_getCpuFeatures());
+  printf("android_getCpuCount()=%d\n", android_getCpuCount());
 #ifdef __arm__
-  printf("android_getCpuIdArm()=%x\n", android_getCpuIdArm());
+  printf("android_getCpuIdArm()=0x%04x\n", android_getCpuIdArm());
 #endif  //__arm__
 }
