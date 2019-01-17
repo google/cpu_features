7c08034ddd4b0076e4a69fec06d93591d5aadee6
Guillaume Chatelet <gchatelet@google.com>
2019-01-17 15:56:49 +0100

    Fix variable declaration and printf formatting

--- ndk_compat/ndk-compat-test.c
+++ ndk_compat/ndk-compat-test.c
@@ -3,7 +3,7 @@
 
 int main() {
   printf("android_getCpuFamily()=%x\n", android_getCpuFamily());
-  printf("android_getCpuFeatures()=%x\n", android_getCpuFeatures());
+  printf("android_getCpuFeatures()=%llx\n", android_getCpuFeatures());
   printf("android_getCpuCount()=%x\n", android_getCpuCount());
 #ifdef __arm__
   printf("android_getCpuIdArm()=%x\n", android_getCpuIdArm());
