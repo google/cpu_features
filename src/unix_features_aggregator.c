9917e8481ef8b7130c8aba6382d7fabb0f1a295d
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2019-01-21 10:39:24 +0100

    Fix hwcaps constants for mips. (#60)
    fixes #59

--- src/unix_features_aggregator.c
+++ src/unix_features_aggregator.c
@@ -28,12 +28,13 @@ void CpuFeatures_SetFromFlags(const size_t configs_size,
 }
 
 static bool IsSet(const uint32_t mask, const uint32_t value) {
+  if (mask == 0) return false;
   return (value & mask) == mask;
 }
 
 static bool IsHwCapsSet(const HardwareCapabilities hwcaps_mask,
                         const HardwareCapabilities hwcaps) {
-  return IsSet(hwcaps_mask.hwcaps, hwcaps.hwcaps) &&
+  return IsSet(hwcaps_mask.hwcaps, hwcaps.hwcaps) ||
          IsSet(hwcaps_mask.hwcaps2, hwcaps.hwcaps2);
 }
 
