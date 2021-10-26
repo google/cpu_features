83158099c6d13fcf9b0f1ad2818357665ba999a8
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-10-27 02:50:50 +0300

    add _M_ARM64 macro for detection arm64

--- include/cpu_features_macros.h
+++ include/cpu_features_macros.h
@@ -39,7 +39,7 @@
 #define CPU_FEATURES_ARCH_ARM
 #endif
 
-#if (defined(__aarch64__) || defined(__arm64__))
+#if (defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64))
 #define CPU_FEATURES_ARCH_AARCH64
 #endif
 
