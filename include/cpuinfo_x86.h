f2a7e9dd41dc93014b0a4628a70b9591ae5ff012
AnvilaWang <98136112+AnvilaWang@users.noreply.github.com>
2022-02-17 11:34:04 +0800

    Update cpuinfo_x86.h
    fix AlignTrailingComments error

--- include/cpuinfo_x86.h
+++ include/cpuinfo_x86.h
@@ -120,53 +120,53 @@ CacheInfo GetX86CacheInfo(void);
 typedef enum {
   X86_UNKNOWN,
   ZHAOXIN_ZHANGJIANG,  // ZhangJiang
-  ZHAOXIN_WUDAOKOU,  // WuDaoKou
-  ZHAOXIN_LUJIAZUI,  // LuJiaZui
-  ZHAOXIN_YONGFENG,  // YongFeng
-  INTEL_80486,       // 80486
-  INTEL_P5,          // P5
-  INTEL_LAKEMONT,    // LAKEMONT
-  INTEL_CORE,        // CORE
-  INTEL_PNR,         // PENRYN
-  INTEL_NHM,         // NEHALEM
-  INTEL_ATOM_BNL,    // BONNELL
-  INTEL_WSM,         // WESTMERE
-  INTEL_SNB,         // SANDYBRIDGE
-  INTEL_IVB,         // IVYBRIDGE
-  INTEL_ATOM_SMT,    // SILVERMONT
-  INTEL_HSW,         // HASWELL
-  INTEL_BDW,         // BROADWELL
-  INTEL_SKL,         // SKYLAKE
-  INTEL_ATOM_GMT,    // GOLDMONT
-  INTEL_KBL,         // KABY LAKE
-  INTEL_CFL,         // COFFEE LAKE
-  INTEL_WHL,         // WHISKEY LAKE
-  INTEL_CNL,         // CANNON LAKE
-  INTEL_ICL,         // ICE LAKE
-  INTEL_TGL,         // TIGER LAKE
-  INTEL_SPR,         // SAPPHIRE RAPIDS
-  INTEL_ADL,         // ALDER LAKE
-  INTEL_RCL,         // ROCKET LAKE
-  INTEL_KNIGHTS_M,   // KNIGHTS MILL
-  INTEL_KNIGHTS_L,   // KNIGHTS LANDING
-  INTEL_KNIGHTS_F,   // KNIGHTS FERRY
-  INTEL_KNIGHTS_C,   // KNIGHTS CORNER
-  INTEL_NETBURST,    // NETBURST
-  AMD_HAMMER,        // K8  HAMMER
-  AMD_K10,           // K10
-  AMD_K11,           // K11
-  AMD_K12,           // K12
-  AMD_BOBCAT,        // K14 BOBCAT
-  AMD_PILEDRIVER,    // K15 PILEDRIVER
-  AMD_STREAMROLLER,  // K15 STREAMROLLER
-  AMD_EXCAVATOR,     // K15 EXCAVATOR
-  AMD_BULLDOZER,     // K15 BULLDOZER
-  AMD_JAGUAR,        // K16 JAGUAR
-  AMD_PUMA,          // K16 PUMA
-  AMD_ZEN,           // K17 ZEN
-  AMD_ZEN_PLUS,      // K17 ZEN+
-  AMD_ZEN2,          // K17 ZEN 2
-  AMD_ZEN3,          // K19 ZEN 3
+  ZHAOXIN_WUDAOKOU,    // WuDaoKou
+  ZHAOXIN_LUJIAZUI,    // LuJiaZui
+  ZHAOXIN_YONGFENG,    // YongFeng
+  INTEL_80486,         // 80486
+  INTEL_P5,            // P5
+  INTEL_LAKEMONT,      // LAKEMONT
+  INTEL_CORE,          // CORE
+  INTEL_PNR,           // PENRYN
+  INTEL_NHM,           // NEHALEM
+  INTEL_ATOM_BNL,      // BONNELL
+  INTEL_WSM,           // WESTMERE
+  INTEL_SNB,           // SANDYBRIDGE
+  INTEL_IVB,           // IVYBRIDGE
+  INTEL_ATOM_SMT,      // SILVERMONT
+  INTEL_HSW,           // HASWELL
+  INTEL_BDW,           // BROADWELL
+  INTEL_SKL,           // SKYLAKE
+  INTEL_ATOM_GMT,      // GOLDMONT
+  INTEL_KBL,           // KABY LAKE
+  INTEL_CFL,           // COFFEE LAKE
+  INTEL_WHL,           // WHISKEY LAKE
+  INTEL_CNL,           // CANNON LAKE
+  INTEL_ICL,           // ICE LAKE
+  INTEL_TGL,           // TIGER LAKE
+  INTEL_SPR,           // SAPPHIRE RAPIDS
+  INTEL_ADL,           // ALDER LAKE
+  INTEL_RCL,           // ROCKET LAKE
+  INTEL_KNIGHTS_M,     // KNIGHTS MILL
+  INTEL_KNIGHTS_L,     // KNIGHTS LANDING
+  INTEL_KNIGHTS_F,     // KNIGHTS FERRY
+  INTEL_KNIGHTS_C,     // KNIGHTS CORNER
+  INTEL_NETBURST,      // NETBURST
+  AMD_HAMMER,          // K8  HAMMER
+  AMD_K10,             // K10
+  AMD_K11,             // K11
+  AMD_K12,             // K12
+  AMD_BOBCAT,          // K14 BOBCAT
+  AMD_PILEDRIVER,      // K15 PILEDRIVER
+  AMD_STREAMROLLER,    // K15 STREAMROLLER
+  AMD_EXCAVATOR,       // K15 EXCAVATOR
+  AMD_BULLDOZER,       // K15 BULLDOZER
+  AMD_JAGUAR,          // K16 JAGUAR
+  AMD_PUMA,            // K16 PUMA
+  AMD_ZEN,             // K17 ZEN
+  AMD_ZEN_PLUS,        // K17 ZEN+
+  AMD_ZEN2,            // K17 ZEN 2
+  AMD_ZEN3,            // K19 ZEN 3
   X86_MICROARCHITECTURE_LAST_,
 } X86Microarchitecture;
 
