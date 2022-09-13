eba1481d43fe2947c15091a95c0703e255f3364f
Marcos <marcos.araque.fiallos@ibm.com>
2022-09-13 20:57:14 +0200

    removed z15 checking

--- src/utils/list_cpu_features.c
+++ src/utils/list_cpu_features.c
@@ -420,10 +420,6 @@ static Node* CreateTree(void) {
   AddMapEntry(root, "platform", CreateString("zSeries"));
   if (strstr(strings.machine, "3931") != NULL)
     AddMapEntry(root, "model", CreateString("z16 Model A01"));
-  else if (strstr(strings.machine, "3932") != NULL)
-    AddMapEntry(root, "model", CreateString("z15 Model T01"));
-  else if (strstr(strings.machine, "8562") != NULL)
-    AddMapEntry(root, "model", CreateString("z15 Model T02"));
   AddMapEntry(root, "machine", CreateString(strings.machine));
   AddMapEntry(root, "# processors", CreateString(strings.num_processors));
   AddMapEntry(root, "instruction", CreateString(strings.type.platform));
