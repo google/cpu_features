8e60d3f9be36e451d23d1e1a0caa6b853c182632
Guillaume Chatelet <gchatelet@google.com>
2024-09-10 13:12:11 +0200

    Forward fix `cpu_features_macros.h` (#364)
    This is a fix for https://github.com/google/cpu_features/pull/363 . The preprocessor check must be done in two steps.

--- include/cpu_features_macros.h
+++ include/cpu_features_macros.h
@@ -234,14 +234,15 @@
 
 #endif  // defined(CPU_FEATURES_ARCH_X86)
 
+#if defined(CPU_FEATURES_ARCH_ANY_ARM)
 // Note: MSVC targeting ARM does not define `__ARM_NEON` but Windows on ARM
 // requires it. In that case we force NEON detection.
-#if defined(__ARM_NEON) || \
-    (defined(CPU_FEATURES_COMPILER_MSC) && defined(CPU_FEATURES_ARCH_ANY_ARM))
+#if defined(__ARM_NEON) || defined(CPU_FEATURES_COMPILER_MSC)
 #define CPU_FEATURES_COMPILED_ANY_ARM_NEON 1
 #else
 #define CPU_FEATURES_COMPILED_ANY_ARM_NEON 0
-#endif
+#endif  //  defined(__ARM_NEON) || defined(CPU_FEATURES_COMPILER_MSC)
+#endif  //  defined(CPU_FEATURES_ARCH_ANY_ARM)
 
 #if defined(CPU_FEATURES_ARCH_MIPS)
 #if defined(__mips_msa)
